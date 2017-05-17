56e2217bdf9cd58016925e83ed1bf20006536b03
v-popras <v-popras@HYDPCM389913D2>
2017-05-17 11:46:09 +0530

    changes for multiselect dropdown

--- CrEST/Microsoft.CrEST.UI/Microsoft.CrEST.UI/webpack.dev.js
+++ CrEST/Microsoft.CrEST.UI/Microsoft.CrEST.UI/webpack.dev.js
@@ -44,7 +44,18 @@ module.exports = {
                     fallbackLoader: "style-loader",
                     loader: "css-loader"
                 })
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
             }
+
         ]
     },
     plugins: [
